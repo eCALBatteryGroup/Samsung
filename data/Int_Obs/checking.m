88e5291a060992acbabc43c7db97148646c190d9
eCALBatteryGroup <satadru86@berkeley.edu>
2016-04-08 14:29:18 -0700

    Add files 3

---
+++ data/Int_Obs/checking.m
@@ -0,0 +1,21 @@
+figure(1)
+plot(time_exp,current_exp)
+hold on
+grid on
+plot(time_exp_new,current_exp_new,'r')
+
+
+
+figure(2)
+plot(time_exp,volt_exp)
+hold on
+grid on
+plot(time_exp_new,volt_exp_new,'r')
+
+
+
+figure(3)
+plot(time_exp,temp_exp)
+hold on
+grid on
+plot(time_exp_new,temp_exp_new,'r')
